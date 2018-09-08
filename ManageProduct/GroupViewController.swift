3bf11ddb48a708708ae1d3ba099338b7b7e49b13
nxan <nxan.developer@gmail.com>
2018-09-08 23:16:48 +0700

    update GroupViewController

--- ManageProduct/GroupViewController.swift
+++ ManageProduct/GroupViewController.swift
@@ -12,7 +12,6 @@ class GroupViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -27,6 +26,17 @@ class GroupViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension GroupViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
 }
 
 
